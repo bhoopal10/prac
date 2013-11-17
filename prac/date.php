cf61c5ff8eb5c1342f0c41247b1babd814d09a68
bhoopal10 <bhoopal10@users.noreply.github.com>
2013-11-17 17:08:01 +0530

    Update date.php

--- prac/date.php
+++ prac/date.php
@@ -1,5 +1,7 @@
 <?php
 $date='2013-03-15';//this is given date string
-echo date('d-m-Y',strtotime($date));// to convert string to date and changeing formate
+echo date('d-m-Y',strtotime($date));//to convert string to date and changing formate
+echo date('d-M-Y',strtotime($date));// to convert string to date and changing formate
 ?>
 ouptput: 15-03-2013
+         15-Mar-2013
