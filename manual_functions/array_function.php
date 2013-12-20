2491e3a5d7c36372e97bc07a70e917678fd9572b
bhoopal10 <bhoopal10@gmail.com>
2013-12-20 16:10:14 +0530

    array value search by key and object array also

---
+++ manual_functions/array_function.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @param $search
+ * @param $arr
+ * @param $key_of_search
+ * @param $name_of_key_to_return_value
+ * @return bool
+ */function get_value_from_array($search,$arr,$key_of_search,$name_of_key_to_return_value)
+{
+    if(is_array($arr))
+    {
+
+        foreach($arr as $values)
+        {
+
+            if($values[$key_of_search] == $search)
+            {
+            return $values[$name_of_key_to_return_value];
+            }
+        }
+    }
+    return false;
+}
+/**
+ * @param $search
+ * @param $arr//Object array
+ * @param $key_of_search
+ * @param $name_of_key_to_return_value
+ * @return bool
+ */function get_value_from_object_array($search,$arr,$key_of_search,$name_of_key_to_return_value)
+{
+    if(is_object($arr))
+    {
+        if($arr->$key_of_search == $search)
+        {
+            return $arr->$name_of_key_to_return_value;
+        }
+    }
+    return false;
+}
+
+?>
